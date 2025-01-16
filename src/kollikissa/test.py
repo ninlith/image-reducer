d67f5603bda3079e8f89cd70d71c9c92f17da9f3
Okko Hartikainen <okko.hartikainen@yandex.com>
2025-01-16 15:47:10 +0200

    Update

--- src/kollikissa/test.py
+++ src/kollikissa/test.py
@@ -8,7 +8,7 @@ MAX_DIMENSION = 1600
 #pillow_heif.register_avif_opener()
 
 # Load the image
-original_image = Image.open("2.jpg")
+original_image = Image.open("input/1.jpg")
 
 # Resize the image to MAX_DIMENSION width or height
 original_width, original_height = original_image.size
@@ -21,20 +21,19 @@ else:
 resized_image = original_image.resize((new_width, new_height), resample=Image.BICUBIC)
 
 # Save the resized image
-resized_image.save("resized_image.png")
+#resized_image.save("output/resized_image.png")
 resized_image.save(
-    "resized_image.avif", 
-    quality=60,             # quality level (0-100)  
-#    speed=0,                # encoding speed (-1 default, 0 slowest, 10 fastest)
-#    range="limited",        # limited range (16-235 for luma, 16-240 for chroma)
+    "output/resized_image.avif", 
+    quality=50,             # quality level (0-100)  
+    speed=0,                # encoding speed (-1 default, 0 slowest, 10 fastest)
+    range="limited",        # limited range (16-235 for luma, 16-240 for chroma)
     subsampling='4:2:0',    # chroma subsampling, 4:0:0 for grayscale
 )
-
-from wand.image import Image
-
-with Image(filename='"resized_image.png"') as original:
-    with original.convert('avif') as converted:
-        converted.save(filename='pikachu.avif')
+resized_image.save(
+    "output/resized_image.webp", 
+    quality=70,             # quality level (0-100)  
+    method=6,
+)
 
 # https://github.com/manoreken2/pillow-avif-plugin-HDR10
 
@@ -46,5 +45,5 @@ with Image(filename='"resized_image.png"') as original:
 #Image.open("resized_image.avif").show()
 
 import os
-print(os.stat("resized_image.avif").st_size)
-#print(os.stat("resized_image.webp").st_size)
+print(os.stat("output/resized_image.avif").st_size)
+print(os.stat("output/resized_image.webp").st_size)
