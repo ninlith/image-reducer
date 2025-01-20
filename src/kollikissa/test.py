48286671a3b4a056f1c5c9606444f934cd4e16e3
Okko Hartikainen <okko.hartikainen@yandex.com>
2025-01-20 14:50:57 +0200

    Fine-tune AVIF settings

--- src/kollikissa/test.py
+++ src/kollikissa/test.py
@@ -1,49 +1,44 @@
 import os
+from pathlib import Path
 import pillow_avif
-#import pillow_heif
-from PIL import Image
+from PIL import Image, ImageOps, UnidentifiedImageError
 
-MAX_DIMENSION = 1600
+def downscale_to_max_dimension(image: Image, max_dimension) -> Image:
+    """Scale the image dimensions down proportionally to fit within a maximum dimension."""
+    width, height = image.size
+    if width <= max_dimension and height <= max_dimension:
+        return image
+    else:
+        return ImageOps.contain(
+            image=image,
+            size=(max_dimension, max_dimension),
+            method=Image.LANCZOS,  # LANCZOS is currently Pillow's highest quality resampling filter
+        )
 
-#pillow_heif.register_avif_opener()
+def compress_image(input_path: Path) -> Path:
+    image = Image.open(input_path)
+    image = downscale_to_max_dimension(image, 1600)
+    exif = image.getexif()
+    output_path = Path("output")/input_path.with_suffix('.avif').name
+    image.save(
+        output_path,
+        format="AVIF",
+        quality=50,             # quality level (0-100)  
+        speed=0,                # encoding speed, slower is better (-1 default, 0 slowest, 10 fastest)
+        range="limited",        # YUV range (full, limited)
+        subsampling='4:2:0',    # chroma subsampling (4:4:4, 4:2:2, 4:2:0, 4:0:0 = grayscale)
+        advanced={
+            "enable-qm": 1,     # quantization matrices (1 enable, 0 disable)
+        },
+        exif=exif,              # Exif data
+        xmp=None,               # XMP data
+        icc_profile=None,       # ICC profile
+    )
+    return output_path
 
-# Load the image
-original_image = Image.open("input/1.jpg")
-
-# Resize the image to MAX_DIMENSION width or height
-original_width, original_height = original_image.size
-if original_width > original_height:
-    new_width = MAX_DIMENSION
-    new_height = int(original_height*(new_width/original_width))
-else:
-    new_height = MAX_DIMENSION
-    new_width = int(original_width*(new_height/original_height))
-resized_image = original_image.resize((new_width, new_height), resample=Image.BICUBIC)
-
-# Save the resized image
-#resized_image.save("output/resized_image.png")
-resized_image.save(
-    "output/resized_image.avif", 
-    quality=50,             # quality level (0-100)  
-    speed=0,                # encoding speed (-1 default, 0 slowest, 10 fastest)
-    range="limited",        # limited range (16-235 for luma, 16-240 for chroma)
-    subsampling='4:2:0',    # chroma subsampling, 4:0:0 for grayscale
-)
-resized_image.save(
-    "output/resized_image.webp", 
-    quality=70,             # quality level (0-100)  
-    method=6,
-)
-
-# https://github.com/manoreken2/pillow-avif-plugin-HDR10
-
-
-
-
-# Display the resized image
-#resized_image.show()
-#Image.open("resized_image.avif").show()
-
-import os
-print(os.stat("output/resized_image.avif").st_size)
-print(os.stat("output/resized_image.webp").st_size)
+for image_path in Path("input").glob('*'):
+    try:
+        compressed_image_path = compress_image(image_path)
+        print(f"{compressed_image_path.stat().st_size} ({compressed_image_path})")
+    except UnidentifiedImageError:
+        print(f"Warning: {image_path} is not a valid image file") 
